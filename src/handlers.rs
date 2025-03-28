cf0cdbb3af7c6c304f167ab34fd37041a82a7552
Renan Gregório <145120433+RenanGreg@users.noreply.github.com>
2025-03-28 08:30:43 -0300

    Update handlers.rs

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,205 +1,100 @@
 use axum::{Json, extract::{State, Path}};
 use sqlx::SqlitePool;
-use serde::{Deserialize, Serialize};
-use crate::middleware::AuthenticatedUser;
-use crate::auth::{hash_senha, verificar_senha, gerar_token};
+use crate::models::{Livro, NovoLivro};
 
-
-
-#[derive(Deserialize)]
-pub struct NovoUsuario {
-    pub nome: String,
-    pub email: String,
-    pub senha: String,
-}
-
-#[derive(Deserialize)]
-pub struct LoginRequest {
-    pub email: String,
-    pub senha: String,
-}
-
-#[derive(Serialize)]
-pub struct AuthResponse {
-    pub message: String,
-    pub token: Option<String>,
-}
-
-
-
-/// Registra usuário no sistema.
-pub async fn register_user(
+/// Cria um novo quadrinho e retorna o registro criado.
+pub async fn create_comic(
     State(pool): State<SqlitePool>,
-    Json(payload): Json<NovoUsuario>,
-) -> Result<Json<AuthResponse>, axum::http::StatusCode> {
-    
-    let senha_hash = hash_senha(&payload.senha)
-        .map_err(|_| axum::http::StatusCode::INTERNAL_SERVER_ERROR)?;
-
-    // Insere o novo usuário no banco de dados com o tipo "usuario".
-    let result = sqlx::query!(
-        "INSERT INTO usuarios (nome, email, senha_hash, tipo) VALUES (?, ?, ?, 'usuario')",
-        payload.nome,
-        payload.email,
-        senha_hash
-    )
-    .execute(&pool)
-    .await;
-
-    match result {
-        Ok(_) => Ok(Json(AuthResponse {
-            message: "Usuário cadastrado com sucesso!".to_string(),
-            token: None,
-        })),
-        Err(_) => Err(axum::http::StatusCode::BAD_REQUEST),
-    }
-}
-
-/// Realiza o login do usuário, retornando um token JWT em caso de sucesso.
-pub async fn login_user(
-    State(pool): State<SqlitePool>,
-    Json(payload): Json<LoginRequest>,
-) -> Result<Json<AuthResponse>, axum::http::StatusCode> {
-    let user = sqlx::query_as!(
-        crate::models::Usuario, 
-        "SELECT * FROM usuarios WHERE email = ?",
-        payload.email
-    )
-    .fetch_optional(&pool)
-    .await
-    .map_err(|_| axum::http::StatusCode::INTERNAL_SERVER_ERROR)?;
-
-    if let Some(user) = user {
-        if verificar_senha(&payload.senha, &user.senha_hash).unwrap_or(false) {
-            let token = gerar_token(&user.email);
-            return Ok(Json(AuthResponse {
-                message: "Login realizado com sucesso!".to_string(),
-                token: Some(token),
-            }));
-        }
-    }
-    Err(axum::http::StatusCode::UNAUTHORIZED)
-}
-
-
-#[derive(Serialize, sqlx::FromRow)]
-pub struct Tarefas {
-    pub id: i32,
-    pub titulo: String,
-    pub descricao: String,
-    pub status: String,
-    pub usuario_id: i32,
-}
-
-#[derive(Deserialize)]
-pub struct NovaTarefa {
-    pub titulo: String,
-    pub descricao: String,
-}
-
-
-
-/// Cria tarefa
-pub async fn create_task(
-    State(pool): State<SqlitePool>,
-    AuthenticatedUser(email): AuthenticatedUser,
-    Json(payload): Json<NovaTarefa>,
-) -> Json<String> {
-    let user_id = sqlx::query!(
-        "SELECT id FROM usuarios WHERE email = ?",
-        email
-    )
-    .fetch_one(&pool)
-    .await
-    .expect("Usuário não encontrado")
-    .id;
-
+    Json(payload): Json<NovoLivro>,
+) -> Json<Livro> {
     sqlx::query!(
-        "INSERT INTO tarefas (titulo, descricao, status, usuario_id) VALUES (?, ?, 'pendente', ?)",
-        payload.titulo, payload.descricao, user_id
+        "INSERT INTO comics (titulo, autor, publicação, genero)
+         VALUES (?, ?, ?, ?,)",
+         payload.titulo,
+         payload.autor,
+         payload.publicação,
+         payload.genero,
     )
     .execute(&pool)
     .await
-    .expect("Erro ao inserir tarefa");
+    .expect("Erro ao inserir o livro");
 
-    Json("Tarefa criada com sucesso!".to_string())
-}
+    // Recupera o id do último registro inserido
+    let comic_id = sqlx::query!("SELECT last_insert_rowid() as id")
+        .fetch_one(&pool)
+        .await
+        .expect("Erro ao recuperar id")
+        .id;
 
-/// Lista as tarefas 
-pub async fn get_tasks(
-    State(pool): State<SqlitePool>,
-    AuthenticatedUser(email): AuthenticatedUser,
-) -> Json<Vec<Tarefa>> {
-    let user_id = sqlx::query!(
-        "SELECT id FROM usuarios WHERE email = ?",
-        email
+    let livro = sqlx::query_as!(
+        Livro,
+        "SELECT * FROM livros WHERE id = ?",
+        livro_id
     )
     .fetch_one(&pool)
     .await
-    .expect("Usuário não encontrado")
-    .id;
+    .expect("Erro ao buscar livro");
 
-    let tarefas = sqlx::query_as!(
-        Tarefa,
-        "SELECT * FROM tarefas WHERE usuario_id = ?",
-        user_id
+    Json(livro)
+}
+
+/// Retorna a lista de todos os quadrinhos.
+pub async fn get_comics(
+    State(pool): State<SqlitePool>,
+) -> Json<Vec<Livro>> {
+    let livro = sqlx::query_as!(
+        Livro,
+        "SELECT * FROM livros"
     )
     .fetch_all(&pool)
     .await
-    .expect("Erro ao buscar tarefas");
+    .expect("Erro ao buscar livros");
 
-    Json(tarefas)
+    Json(livros)
 }
 
-/// Atualiza tarefa.
-pub async fn update_task(
+/// Atualiza um quadrinho existente com base no ID e nos dados fornecidos.
+pub async fn update_comic(
     State(pool): State<SqlitePool>,
-    AuthenticatedUser(email): AuthenticatedUser,
     Path(id): Path<i32>,
-    Json(payload): Json<NovaTarefa>,
-) -> Json<String> {
-    let user_id = sqlx::query!(
-        "SELECT id FROM usuarios WHERE email = ?",
-        email
-    )
-    .fetch_one(&pool)
-    .await
-    .expect("Usuário não encontrado")
-    .id;
-
+    Json(payload): Json<NovoLivro>,
+) -> Json<Livro> {
     sqlx::query!(
-        "UPDATE tarefas SET titulo = ?, descricao = ? WHERE id = ? AND usuario_id = ?",
-        payload.titulo, payload.descricao, id, user_id
+        "UPDATE comics SET titulo = ?, autor = ?, publicação = ?, genero = ?, WHERE id = ?",
+        payload.titulo,
+        payload.autor,
+        payload.publicação,
+        payload.genero,
+        id
     )
     .execute(&pool)
     .await
-    .expect("Erro ao atualizar tarefa");
+    .expect("Erro ao atualizar livro");
 
-    Json("Tarefa atualizada com sucesso!".to_string())
-}
-
-/// Deleta uma tarefa
-pub async fn delete_task(
-    State(pool): State<SqlitePool>,
-    AuthenticatedUser(email): AuthenticatedUser,
-    Path(id): Path<i32>,
-) -> Json<String> {
-    let user_id = sqlx::query!(
-        "SELECT id FROM usuarios WHERE email = ?",
-        email
+    let livro = sqlx::query_as!(
+        Livro,
+        "SELECT * FROM livro WHERE id = ?",
+        id
     )
     .fetch_one(&pool)
     .await
-    .expect("Usuário não encontrado")
-    .id;
+    .expect("Erro ao buscar livro");
 
+    Json(livro)
+}
+
+/// Deleta um quadrinho pelo ID.
+pub async fn delete_comic(
+    State(pool): State<SqlitePool>,
+    Path(id): Path<i32>,
+) -> Json<String> {
     sqlx::query!(
-        "DELETE FROM tarefas WHERE id = ? AND usuario_id = ?",
-        id, user_id
+        "DELETE FROM livros WHERE id = ?",
+        id
     )
     .execute(&pool)
     .await
-    .expect("Erro ao deletar tarefa");
+    .expect("Erro ao deletar livro");
 
-    Json("Tarefa deletada com sucesso!".to_string())
+    Json(format!("Comic com id {} removida", id))
 }
